0f6f2c2a3e6079dd75f16c4901b7d79bfc7851fe
Natasa Cvrlja <natasa.cvrlja@gmail.com>
2023-06-19 15:23:14 +0200

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroComponent } from './hero/hero.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "", component: HeroComponent
+}, {
+  path: "about-page", component: AboutPageComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ContentComponent } from './content/content.component';
+import { InfoComponent } from './info/info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    AboutPageComponent,
+    ContentComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
